e37d0e7a1ebba7a2f0514b09b6d915c6fdfddc6d
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-01-22 17:12:19 +0400

    commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+let scrollContainer=document.querySelector(".carousel-cards");
+let nextBtn=document.getElementById("nextBtn");
+
+nextBtn.addEventListener("click", () => {
+    const cardWidth = document.querySelector(".card").offsetWidth;
+
+    const totalWidth = scrollContainer.scrollWidth;
+
+    if (scrollContainer.scrollLeft + scrollContainer.offsetWidth >= totalWidth) {
+        scrollContainer.scrollLeft = 0;
+    } else {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += totalWidth;
+    }
+});
+
